ec3367f127f27d6b6cb98c6936afff1cbfa60bc5
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2021-09-18 17:52:35 -0500

    parsing the weather station data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # docker-alpine-ambient-weather-mqtt-publisher
+
+Protocl Spec is at <https://help.ambientweather.net/help/advanced/>
---
+++ local-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker build \
+        --no-cache \
+        --pull \
+        --file Dockerfile \
+        --tag weather \
+        .
+
+MSYS_NO_PATHCONV=1 \
+    docker run -it \
+        --env TZ=America/Chicago \
+        --env MQTT_SERVER="192.168.8.21" \
+        --env MQTT_SERVER_PORT="1883" \
+        --publish 80:80 \
+        weather
